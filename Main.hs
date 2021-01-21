726f7be452918116219e89d4b3a9db1dca7c78fe
Tom Goring <tgoring97@gmail.com>
2021-01-21 03:17:52 +0000

    Complete exercise to duplicate all elements of list

--- Main.hs
+++ Main.hs
@@ -2,21 +2,7 @@
 
 module Main where
 
-data ListItem a = Single a | Multiple Int a
-    deriving Show
+dupli :: [a] -> [a]
+dupli = concatMap(replicate 2)
 
-encode :: Eq a => [a] -> [ListItem a]
-encode [] = []
-encode (x:xs) = g f : encode (dropWhile (==x) xs)
-    where 
-        f = (length (x : takeWhile (==x) xs), x)
-        g (1, x) = Single x
-        g (n, x) = Multiple n x
-
-decode :: Eq a => [ListItem a] -> [a]
-decode = concatMap decodeHelper
-    where
-        decodeHelper (Single x) = [x]
-        decodeHelper (Multiple n x) = replicate n x
-
-main = print(encode "aaaabbbccaadeeeee")
+main = print(dupli [1, 2, 3])
