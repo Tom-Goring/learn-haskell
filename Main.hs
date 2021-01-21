d1819e8f743df84fed2bf4f4c5185ffdd46b814a
Tom Goring <tgoring97@gmail.com>
2021-01-21 18:55:13 +0000

    Finish exercise 18

--- Main.hs
+++ Main.hs
@@ -2,7 +2,11 @@
 
 module Main where
 
-split :: [a] -> Int -> ([a], [a])
-split xs n = ([x | (x, i) <- zip xs [0, 1..], i < n], [x | (x, i) <- zip xs [0, 1..], i >= n])
+slice :: [a] -> Int -> Int -> [a]
+slice [] _ _ = []
+slice xs i j
+  | i == j = []
+  | i > j = error "Cannot produce slice of negative length"
+  | otherwise = [x | (x, k) <- zip xs [0, 1..], k >= i && k <= j]
 
-main = print(split "abcdefghik" 3)
+main = print(slice "abcdefghik" 2 4)
