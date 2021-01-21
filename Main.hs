ca2553d91639260dbf3a6fad30a1830540bb7275
Tom Goring <tgoring97@gmail.com>
2021-01-21 02:40:10 +0000

    Encode run-length using special data type

--- Main.hs
+++ Main.hs
@@ -2,12 +2,21 @@
 
 module Main where
 
+data ListItem a = Single a | Multiple Int a
+    deriving Show
+
 pack :: Eq a => [a] -> [[a]]
 pack [] = []
 pack (x:xs) = (x : takeWhile (==x) xs) : pack(dropWhile (==x) xs)
 
-encode :: Eq a => [a] -> [(Int, a)]
-encode [] = []
-encode xs = [(length x, head x) | x <- pack xs]
+encode' :: Eq a => [a] -> [(Int, a)]
+encode' [] = []
+encode' xs = [(length x, head x) | x <- pack xs]
+
+encode :: Eq a => [a] -> [ListItem a]
+encode = map encodeHelper . encode'
+    where
+        encodeHelper (1, x) = Single x
+        encodeHelper (n, x) = Multiple n x
 
 main = print(encode "aaaabbbbcddddd")
