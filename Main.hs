e3621b1ea1bdf4d951012f855a1c337511c19723
Tom Goring <tgoring97@gmail.com>
2021-01-21 03:08:39 +0000

    Refactor q11 to use direct encoding rather than pack for q13

--- Main.hs
+++ Main.hs
@@ -5,19 +5,13 @@ module Main where
 data ListItem a = Single a | Multiple Int a
     deriving Show
 
-pack :: Eq a => [a] -> [[a]]
-pack [] = []
-pack (x:xs) = (x : takeWhile (==x) xs) : pack(dropWhile (==x) xs)
-
-encode' :: Eq a => [a] -> [(Int, a)]
-encode' [] = []
-encode' xs = [(length x, head x) | x <- pack xs]
-
 encode :: Eq a => [a] -> [ListItem a]
-encode = map encodeHelper . encode'
-    where
-        encodeHelper (1, x) = Single x
-        encodeHelper (n, x) = Multiple n x
+encode [] = []
+encode (x:xs) = g f : encode (dropWhile (==x) xs)
+    where 
+        f = (length (x : takeWhile (==x) xs), x)
+        g (1, x) = Single x
+        g (n, x) = Multiple n x
 
 decode :: Eq a => [ListItem a] -> [a]
 decode = concatMap decodeHelper
@@ -25,4 +19,4 @@ decode = concatMap decodeHelper
         decodeHelper (Single x) = [x]
         decodeHelper (Multiple n x) = replicate n x
 
-main = print(decode [Multiple 4 'a', Single 'b', Multiple 2 'c'])
+main = print(encode "aaaabbbccaadeeeee")
