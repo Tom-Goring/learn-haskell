98f0df75169620678dd870382430f9942c5b5d83
Tom Goring <tgoring97@gmail.com>
2021-01-21 18:42:45 +0000

    Finish exercise 17 (but with predicates)

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-dropEvery :: [a] -> Int -> [a]
-dropEvery xs n = [x | (x, idx) <- zip xs [0, 1..], mod (idx+1) n /= 0 ]
+split :: [a] -> Int -> ([a], [a])
+split xs n = ([x | (x, i) <- zip xs [0, 1..], i < n], [x | (x, i) <- zip xs [0, 1..], i >= n])
 
-main = print(dropEvery "abcdefghik" 3)
+main = print(split "abcdefghik" 3)
